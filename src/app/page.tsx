601e352ee3788a64d6cf91459ae146c189b9a73b
Daniel Lage <daniellc3221@gmail.com>
2024-08-22 12:04:38 -0300

    start

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-slate-950 text-white">
+      Hello!
+    </main>
+  );
+}
